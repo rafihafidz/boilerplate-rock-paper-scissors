0fd0fcaa7482b4e5f705776a1f92415c796850a3
rafihafidz Kurniantoko <rafihafidzk@gmail.com>
2025-07-08 17:11:10 +0000

    Completed RPS challenge with working AI

--- RPS.py
+++ RPS.py
@@ -1,10 +1,34 @@
-# The example function below keeps track of the opponent's history and plays whatever the opponent played two plays ago. It is not a very good player so you will need to change the code to pass the challenge.
+def player(prev_play, opponent_history=[], play_order={}):
+    if prev_play:
+        opponent_history.append(prev_play)
 
-def player(prev_play, opponent_history=[]):
-    opponent_history.append(prev_play)
+    # Inisialisasi strategi berdasarkan deteksi lawan
+    n = 3
+    if len(opponent_history) < n:
+        return "R"
 
-    guess = "R"
-    if len(opponent_history) > 2:
-        guess = opponent_history[-2]
+    # Simpan sekuens terakhir lawan untuk pola
+    last_moves = "".join(opponent_history[-n:])
 
-    return guess
+    if last_moves not in play_order:
+        play_order[last_moves] = {"R": 0, "P": 0, "S": 0}
+
+    if len(opponent_history) > n:
+        prev_seq = "".join(opponent_history[-n - 1 : -1])
+        play_order[prev_seq][opponent_history[-1]] += 1
+
+    # Prediksi langkah lawan selanjutnya berdasarkan frekuensi
+    possible = play_order[last_moves]
+    prediction = max(possible, key=possible.get)
+
+    # Balas dengan langkah yang mengalahkan prediksi
+    return counter_move(prediction)
+
+
+def counter_move(move):
+    if move == "R":
+        return "P"
+    elif move == "P":
+        return "S"
+    elif move == "S":
+        return "R"
